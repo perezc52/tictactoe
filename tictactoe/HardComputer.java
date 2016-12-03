74b90dba2172a523b389177fd34545f884d69658
ushanthaster <ushanthaster@gmail.com>
2016-12-03 15:51:05 -0600

    ignore the hardcomp

--- tictactoe/HardComputer.java
+++ tictactoe/HardComputer.java
@@ -12,6 +12,11 @@ import java.io.Serializable;
  * @author lucy
  */
 public class HardComputer extends ComputerPlayer implements Serializable, Movable {
+   
+    int emptySquares;
+    int rowScore;
+    private int scores [][];
+    
     public HardComputer()
     {
         super("HardComputer");
@@ -22,4 +27,35 @@ public class HardComputer extends ComputerPlayer implements Serializable, Movabl
     {
         return null;
     }
+
+    
+    /*defense*/
+    /*check for 2 or 3 in a row or column occupied squares by the opponent*/
+   public int setMove(int currentPlayer, int rowScore)
+   {
+   if(currentPlayer == 1){
+       if(rowScore == 1 && emptySquares == 3 )||(rowScore == 2 && emptySquares == 2 )|| (rowScore == 3 && emptySquares == 1 ){
+          for(int x = 0; x <= 4; x++){
+            x = rowScore;  
+              if (rowscore == 0){
+                  row[i] = 1;
+                }
+            }
+        }
+   }  
+       
+   
+   if(currentPlayer == 2){
+       if(rowScore == 2 && emptySquares == 3 )||(rowScore == 4 && emptySquares == 2 )|| (rowScore == 6 && emptySquares == 1 ){
+          for(int x = 0; x <= 4; x++){
+            x = rowScore;  
+              if (rowscore == 0){
+                  row[i] = 2;
+                }
+            }
+        }
+   } 
+   
+
+
 }
--- tictactoe/MediumComputer.java
+++ tictactoe/MediumComputer.java
@@ -6,21 +6,138 @@
 package tictactoe;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import static tictactoe.MainMenu.NUMBER_OF_COLUMNS;
+import static tictactoe.MainMenu.NUMBER_OF_ROWS;
+import javafx.scene.control.Button;
+import static tictactoe.MainMenu.NUMBER_OF_SCORES;
+
 
 /**
+ *author @usha
  *
- * @author lucy
  */
 public class MediumComputer extends ComputerPlayer implements Serializable, Movable{
-    public MediumComputer()
+
+/*current player for the medium AI*/
+int currentPlayer;
+/*occupied player squares*/
+private int score[][];
+/*empty squares*/
+private ArrayList<Position> openSquares;
+public static final int NUMBER_OF_ROWS = 5;
+public static final int NUMBER_OF_COLUMNS = 5;
+/*score of total number of squares*/
+public static final int NUMBER_OF_SCORES = 2 * NUMBER_OF_ROWS + 2;
+    
+/* player 1 or 2 as AI */
+    public MediumComputer(int player)
     {
         super("MediumComputer");
-        
+        this.currentPlayer= player;
+        /*create a new empty array list*/
+        openSquares = new ArrayList<>();
     }
-    
+   
     @Override
     public Position makeMove(byte [][] allSquares)
     {
         return null;
     }
+    
+    
+  /*modifier for setting a new empty board */
+    public void setOpenSquares(byte [][] allSquares){
+    openSquares.clear();
+    
+     for (int i = 0; i < NUMBER_OF_ROWS; i++)
+        {
+            for (int j = 0; j < NUMBER_OF_COLUMNS; j++)
+            {
+                /*set squares to 0*/
+                if (allSquares[i][j] == 0)
+                    openSquares.add(new Position(i, j));
+            }
+        }
+    }
+    
+    /* attack : check for 2 or 3 in a row or column sqaures occupied squares by the opponent*/
+   public int setMove(int currentPlayer, int score[i][j])
+   {
+    if(currentPlayer == 2)
+    {
+       for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < NUMBER_OF_SCORES; j++)
+                {  
+                    /* access the row or column that has a absoulte score of 2 or 3  */
+                   if (Math.abs(scores[i][j]) == 2 )
+                    {
+                        /*get the 2nd empty square box in a row or column*/
+                       for (int x = 0; x >= NUMBER_OF_SCORES; x++ )
+                        {
+                            if (i == 0 && j == 1)
+                             {
+                                /*occupy by the square by player 2*/
+                                 x = -1;
+                            }             
+                        }   
+                    }
+                       /*if 3 in a row occupied by player 1*/                      
+                    else if(Math.abs(scores[i][j]) == 3)
+                     {
+                        for (int x = 0; x >=NUMBER_OF_SCORES;x++ )
+                         { 
+                           if (i == 0 && j == 1)
+                            {
+                             x = -1;   
+                            }
+                         }
+                     }
+         }
+   }
+  
+   else if(currentPlayer == 1)
+    {
+       for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < NUMBER_OF_SCORES; j++)
+                {  
+                    /* access the row or column that has a absoulte score of 2 or 3  */
+                   if (Math.abs(scores[i][j]) == -2 )
+                    {
+                        /*get the 2nd empty square box in a row or column*/
+                       for (int x = 0; x >= NUMBER_OF_SCORES; x++ )
+                        {
+                            if (i == 0 && j == 1)
+                             {
+                                /*occupy by the square by player 1*/
+                                 x = 1;
+                            }             
+                        }   
+                    }
+                       /*if 3 in a row occupied by player 2*/                      
+                    else if(Math.abs(scores[i][j]) == -3)
+                     {
+                        for (int x = 0; x >=NUMBER_OF_SCORES;x++ )
+                         { 
+                           if (i == 0 && j == 1)
+                            {
+                             x = 1;   
+                            }
+                         }
+                     }
+         }
+   }
+  
+   }
+    
+   
+   
+    
+    
+    
+    
+    
+    
 }
